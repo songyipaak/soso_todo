79a7a10cb01eba5a96312e3003cec1c8d6c32fbd
songyipaak <80510808+songyipaak@users.noreply.github.com>
2021-06-11 14:44:01 +0900

    Update todo.js

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,5 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("#todo-form input");
-// id가 todo-form인 form 안에서 input을 찾는 것
 const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
@@ -9,15 +8,12 @@ let toDos = [];
 
 function saveToDos() {
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
-    // JSON.stringify(value)는 value를 string으로 저장시켜주는 JSON 기능
 }
 
 function deleteToDo(event) {
     const li = event.target.parentElement;
     li.remove();
     toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id));
-    // 클릭했던 li의 id를 갖고 있는 toDo를 지우기
-    // li.id는 string type이고 todo.id는 number 타입이기에 int형으로 형변환
     saveToDos();
 }
 
@@ -40,14 +36,12 @@ function paintToDo(newToDo) {
 function handleToDoSubmit(event) {
     event.preventDefault();
     const newToDo = toDoInput.value;
-    // toDoInput.value를 newToDo에 복사하는 것, 그 이후엔 newToDo 변수와 상관x
     toDoInput.value = "";
 
     const newToDoObj = {
         text: newToDo,
         id: Date.now(),
     };
-    // id로 각각의 li item을 구별하기 위해
     toDos.push(newToDoObj);
     paintToDo(newToDoObj);
 
